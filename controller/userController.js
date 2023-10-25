4c52482fc2edc787ab71aaf3e8c9f88841140a20
Thafsal <thafsalthafsu@gmail.com>
2023-10-25 23:56:50 +0530

    user controller added

--- controller/userController.js
+++ controller/userController.js
@@ -1,17 +1,63 @@
-const User = require('../model/userModel')
-
+const User = require("../model/userModel");
 
 //Landing page get route
 
-const landingPage = async(req,res)=>{
-    try {
-        if(!req.session){
-            res.render('index')
-        }else{
-            res.render('home')
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({message:error.message})
+const getUserLanding = (req, res) => {
+  if (!req.session) {
+    res.render("index");
+  } else {
+    res.render("home");
+  }
+};
+
+//Login route
+
+const getUserLogin = (req, res) => {
+  res.render("login");
+};
+
+//Signup route
+
+const getUserSignup = (req, res) => {
+  res.render("signup");
+};
+
+//Home route
+
+const getUserHome = (req, res) => {
+  if (req.session) {
+    res.render("home");
+  } else {
+    res.render("index");
+  }
+};
+
+//About route
+
+const getUserAbout = (req, res) => {
+  if (req.session) {
+    res.render("about");
+  } else {
+    res.render("index");
+  }
+};
+
+//Contact route
+
+const getUserContact = (req, res) => {
+  if (req.session) {
+    res.render("contact");
+  } else {
+    res.render("index");
+  }
+};
+
+//Products route
+
+const getUserProducts = (req,res)=>{
+    if(!req.session){
+        res.render('products')
+    }else{
+        res.render('index')
     }
 }
